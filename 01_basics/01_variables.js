43eaabdfcf8c689532446a615aa0cd96d49d146b
SAGAR SEN <142751793+sagarsen07@users.noreply.github.com>
2023-11-24 08:51:54 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,18 @@
-const accoundId = 144553
+const accountId = 144553
 let accountEmail = "sagar@google.com"
 var accountPassword = " 12345"
 accountCity = "Ghaziabad"
+let accountState
 
-accountId = 2
+//accountId = 2 // not allowed to change the value of a constant 
+accountEmail = "sen@xyz.com"
+accountPassword = "gdsdjflkjs"
+accountCity = "noida"
 
 console.log(accountId);
+
+console.table ([accountEmail, accountId, accountPassword, accountCity, accountState])
+
+/*
+prefer not to use var due to issues in block scope
+*/
