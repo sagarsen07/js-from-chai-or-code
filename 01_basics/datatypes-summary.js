058f1e69a89295835e6f4f3083f934dd2db0dfdb
SAGAR SEN <142751793+sagarsen07@users.noreply.github.com>
2023-11-26 11:01:05 +0000

    stack and heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -32,6 +32,30 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack (Primitive),  Heap (Non-Primitive)
+
+let myYoutubeName = "Jacksen Talks";
+let anotherName = myYoutubeName;
+anotherName = "Jacksen Vlogs";
+
+console.log (myYoutubeName);
+console.log (anotherName);
+
+let userOne = {
+    email: "sagar@xyz",
+    upi: "sagar@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "user@google.com";
+
+console.log (userOne.email);
+console.log (userTwo.email);
