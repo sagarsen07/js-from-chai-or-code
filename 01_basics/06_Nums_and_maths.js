9b3f25dac4fbb44e7dd41440b737eed0386531a7
SAGAR SEN <142751793+sagarsen07@users.noreply.github.com>
2023-11-29 00:50:54 +0000

    Math and Num

--- 01_basics/06_Nums_and_maths.js
+++ 01_basics/06_Nums_and_maths.js
@@ -1,15 +1,36 @@
 const score = 400
 
 const balance = new Number(100);
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString()); // used to convert the variable into string
+// console.log(balance.toString()); // used to convert the variable into string
 
-console.log(balance.toFixed(3));  // used to fix the number of decimal values in a variable
+// console.log(balance.toFixed(3));  // used to fix the number of decimal values in a variable
 
 const otherNumber = 23.4541;
-console.log(otherNumber.toPrecision(3))
+// console.log(otherNumber.toPrecision(3))
 
 const hundreds = 1000000;
-console.log(hundreds.toLocaleString());
-console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++ MATHS ++++++++++++++ //
+
+// console.log(Math);
+// console.log(Math.abs(-23));
+// console.log(Math.round(5.71325));  // used for round off
+// console.log(Math.ceil(4.11111));  // round off to upper limit
+// console.log(Math.floor(4.99999));  // round off to lower limit
+// console.log(Math.min(2,58,6,8,4));
+// console.log(Math.max(3,52,8,4,6,9,4));
+
+console.log (Math.random());
+console.log (Math.random() * 10 + 1);
+console.log ((Math.random() * 10) + 1);
+console.log (Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min );
