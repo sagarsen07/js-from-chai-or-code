826f517ceba4b99cd440b0184c2292b8718ee394
SAGAR SEN <142751793+sagarsen07@users.noreply.github.com>
2023-11-29 01:36:19 +0000

    dates started

---
+++ 01_basics/07_datesInJS.js
@@ -0,0 +1,11 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleString('en-IN'));
+
+console.log(typeof myDate)
+
+let myCreatedDate
