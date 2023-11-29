747f1fcb2c94c1da806066cf2b6faf3c3d40b101
SAGAR SEN <142751793+sagarsen07@users.noreply.github.com>
2023-11-29 09:27:01 +0000

    Date and Time complete

--- 01_basics/07_datesInJS.js
+++ 01_basics/07_datesInJS.js
@@ -1,11 +1,34 @@
 // Dates
 
 let myDate = new Date()
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString());
-console.log(myDate.toLocaleString('en-IN'));
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleString('en-IN'));
 
-console.log(typeof myDate)
+// console.log(typeof myDate)
 
-let myCreatedDate
+//let myCreatedDate = new Date(2023, 0, 12);
+//console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate = new Date(2023, 0, 12, 6, 23);
+// console.log(myCreatedDate.toLocaleString())
+
+ let myCreatedDate = new Date("01-12-2023");
+// console.log(myCreatedDate.toLocaleString())
+
+let myTampStamp = Date.now()
+
+// console.log(myTampStamp)
+// console.log(myCreatedDate.getTime())
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+// console.log(newDate)
+// console.log(newDate.getMonth()+1)
+// console.log(newDate.getDay())
+
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
