5ea54258979574725fb1f13b4829e2183dbd91c7
SAGAR SEN <142751793+sagarsen07@users.noreply.github.com>
2023-11-30 18:32:36 +0000

    arrays completed

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -9,4 +9,22 @@ const dc_heroes = ["superman", "aquaman", "batman"]
 // console.log(marvel_heroes)   // it still gives the same result because concat give the result in new array so we have to store the result in new array to get the desired output
 
 const all_heroes = marvel_heroes.concat(dc_heroes)
-console.log(all_heroes) 
+// console.log(all_heroes) 
+
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name: "hitesh"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
