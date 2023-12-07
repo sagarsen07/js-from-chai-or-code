6b806820ed8ab03c0b4b711683f7c929f2185893
SAGAR SEN <142751793+sagarsen07@users.noreply.github.com>
2023-12-07 01:25:40 +0000

    almost done

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,35 @@
+// objects can be declared in two ways (1)literals type, (2) constructor type
+
+// singleton (constructor se banane pe singleton banta hai)
+
+// Object Literals
+
+const mySym = Symbol("key1")
+
+const jsUser = {
+    name: "Sagar",
+    "full name" : "Sagar Sen",
+    [mySym] : "mykey1",
+    age: 23,
+    location: "Ghaziabad",
+    email: "sagar@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// how to access objects?
+console.log(jsUser.email);
+console.log(jsUser["email"]);
+console.log(jsUser["full name"]);
+console.log(jsUser[mySym]);
+console.log(typeof jsUser[mySym]);
+
+// changing the values of the objects
+jsUser.email = "sagarsen@google.com";
+Object.freeze(jsUser)   // now no changes can be done in the object
+jsUser.email = "sen@google.com";
+
+console.log(jsUser)
+
+
+// in javaScript functions are treated same as variables
