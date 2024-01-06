038fc6b6cbf1ebff172644b97574f75c0e137549
SAGAR SEN <142751793+sagarsen07@users.noreply.github.com>
2024-01-06 05:05:41 +0000

    revision

--- 01_basics/06_Nums_and_maths.js
+++ 01_basics/06_Nums_and_maths.js
@@ -8,7 +8,7 @@ const balance = new Number(100);
 // console.log(balance.toFixed(3));  // used to fix the number of decimal values in a variable
 
 const otherNumber = 23.4541;
-// console.log(otherNumber.toPrecision(3))
+//console.log(otherNumber.toPrecision(4))
 
 const hundreds = 1000000;
 // console.log(hundreds.toLocaleString());
@@ -25,12 +25,12 @@ const hundreds = 1000000;
 // console.log(Math.min(2,58,6,8,4));
 // console.log(Math.max(3,52,8,4,6,9,4));
 
-console.log (Math.random());
-console.log (Math.random() * 10 + 1);
-console.log ((Math.random() * 10) + 1);
-console.log (Math.floor(Math.random() * 10) + 1);
+// console.log (Math.random());
+// console.log (Math.random() * 10 + 1);
+// console.log ((Math.random() * 10) + 1);
+// console.log (Math.floor(Math.random() * 10) + 1);
 
 const min = 10;
 const max = 20;
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min );
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min );
