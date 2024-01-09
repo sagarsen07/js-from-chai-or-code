a4ef93d82cb7c7ff71e1c7e68e33653504da962d
SAGAR SEN <142751793+sagarsen07@users.noreply.github.com>
2024-01-09 06:29:42 +0000

    revision

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -8,8 +8,10 @@ const dc_heroes = ["superman", "aquaman", "batman"]
 // marvel_heroes.concat(dc_heroes)
 // console.log(marvel_heroes)   // it still gives the same result because concat give the result in new array so we have to store the result in new array to get the desired output
 
-const all_heroes = marvel_heroes.concat(dc_heroes)
-// console.log(all_heroes) 
+// const all_heroes = marvel_heroes.concat(dc_heroes)
+// Or
+const all_heroes = [...marvel_heroes, ...dc_heroes]
+console.log(all_heroes)
 
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
@@ -19,12 +21,12 @@ const real_another_array = another_array.flat(Infinity)
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+// console.log(Array.isArray("Hitesh"))
+// console.log(Array.from("Hitesh"))
+// console.log(Array.from({name: "hitesh"})) // interesting
 
-let score1 = 100
-let score2 = 200
-let score3 = 300
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+// console.log(Array.of(score1, score2, score3));
