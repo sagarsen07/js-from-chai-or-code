bacde998005a011d6534e19501543ca035e1948c
SAGAR SEN <142751793+sagarsen07@users.noreply.github.com>
2024-02-07 23:43:30 +0530

    Add files via upload

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,37 @@
+//*************** if condition
+
+// if (condition){
+//     // condition should be true if we want to execute the code inside the if scope
+// }
+
+// comparision operators
+//  < , > , <= , >= , == , != , === , !==
+
+
+// if (2 == "2"){
+//     console.log("executed");
+// }
+// if (2 === "2"){
+//     console.log("executed");
+// }
+
+
+// const temprature = 51;
+// if (temprature < 50){
+//     console.log("temprature is less than 50");
+// }
+// else {
+//     console.log("temprature greater than 50");
+// }
+
+
+// ******** short hand notation (implicit scope)
+// let balance = 1000
+// if (balance > 500) console.log("test");
+// //multiple statements can also be executed by adding comma (,)
+// if (balance > 500) console.log("test"), console.log("test 2");
+
+
+
+
+// all the if else rule are exactly same as in C++
