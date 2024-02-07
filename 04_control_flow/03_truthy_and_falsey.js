3e58c66f81c39716b5b0ffbd0eb0343cfa5613b7
SAGAR SEN <142751793+sagarsen07@users.noreply.github.com>
2024-02-08 00:39:01 +0530

    Add files via upload

---
+++ 04_control_flow/03_truthy_and_falsey.js
@@ -0,0 +1,31 @@
+// const userEmail = "sagar@gmail.com";
+
+// if (userEmail){
+//     console.log("got user Email")
+// } else {
+//     console.log("don't have user Email");
+// }
+
+
+// Falsey values:
+// false, 0, -0 , BigInt ,  0n , "" , null , undefined , NaN
+
+
+//Truthey Vlaues:
+//non falsey values are truthy but some of the amazing nes are as fallowss:
+// "0" , 'false' , " " , [], {} , function(){}
+
+
+
+
+
+//Nullish Coslesing Operator (??): null undefined
+//samajh nhi aaya...
+
+
+
+// Terniary Operator
+// condition? true:false
+
+// const price = 500;
+// price < 1000? console.log("yes"): console.log("No");
