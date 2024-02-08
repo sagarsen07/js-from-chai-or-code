18951d921493a160aa5f7aa3103273eb45d2602d
SAGAR SEN <142751793+sagarsen07@users.noreply.github.com>
2024-02-08 12:26:50 +0530

    Add files via upload

---
+++ 05_iterations/01_one.js
@@ -0,0 +1,43 @@
+// for
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element);
+// }
+
+
+// for (let i = 0 ; i <= 10 ; i++){
+//     const element = i;
+//     if (element == 5){
+//         console.log("5 is the best number");
+//     }
+//     console.log(element);
+// }
+
+
+// nested loop
+// for (let i = 0 ; i <= 10 ; i++){
+//     for (let j = 0 ; j <= 10 ; j++){
+//         console.log(`inner loop value is ${j} and outer loop value is ${i}`);
+//     }
+// }
+
+
+
+// break and continue
+
+// for (let i = 1 ; i <= 10 ; i++){
+//     console.log(`value if i is ${i}`);
+//     if (i == 5){
+//         console.log("5 detected");
+//         break;
+//     }
+// }
+
+// for (let i = 1 ; i <= 10 ; i++){
+//     if (i == 5){
+//         console.log("5 detected");
+//         continue;
+//     }
+//     console.log(`value if i is ${i}`);
+// }
