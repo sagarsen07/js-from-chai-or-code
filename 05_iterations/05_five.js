b04c5f182d7897c7326229805a88ef2cc1a8b0eb
SAGAR SEN <142751793+sagarsen07@users.noreply.github.com>
2024-02-08 12:29:05 +0530

    Add files via upload

---
+++ 05_iterations/05_five.js
@@ -0,0 +1,41 @@
+// **************** FOR EACH LOOP ******************* \\
+
+const coding = ["js", "ruby", "python", "java", "cPlusPlus"];
+
+// coding.forEach( function (i) {      // function name is not required in this as it is a call back function
+//     console.log(i);
+// } )
+
+// coding.forEach( (i) => {
+//     console.log(i);
+// } )
+
+// function printme (item) {
+//     console.log(item);
+// }
+
+// coding.forEach(printme);
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);
+// } )
+
+
+const myCoding = [
+    {
+        languageName: "javaScript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "Python",
+        languageFileName: "py"
+    },
+]
+
+// myCoding.forEach ( (item) => {
+//     console.log(item.languageName);
+// })
