c594092d5f7408671ccc8955bfa8affbc98985b1
SAGAR SEN <142751793+sagarsen07@users.noreply.github.com>
2024-02-08 12:27:52 +0530

    Add files via upload

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,32 @@
+//********** while loop
+
+// let i = 0;
+// while (i <= 10){
+//     console.log(`value of index is ${i}`);
+//     i = i+2;
+// }
+
+// let myArray = ['flash' , "batman" , "superman"];
+// let i = 0;
+// while (i < myArray.length) {
+//     console.log(`value is ${myArray[i]}`);
+//     i++;
+// }
+
+
+
+
+// ***********DO WHILE LOOP
+
+// let score = 1;
+// do {
+//     console.log(`value is ${score}`);
+//     score++;
+// } while (score <= 10)
+
+
+// let score = 11;
+// do {
+//     console.log(`value is ${score}`);
+//     score++;
+// } while (score <= 10)
