2919c7a3f52195aed181f84d6d83238e7b99e2ff
SAGAR SEN <142751793+sagarsen07@users.noreply.github.com>
2024-02-10 01:41:20 +0530

    Add files via upload

---
+++ 05_iterations/08_eight.js
@@ -0,0 +1,34 @@
+const myNum = [1, 2, 3];
+
+// const myTotal = myNum.reduce(function(accumulator, currentValue) {
+//     console.log(`accumulator: ${accumulator} ans current value: ${currentValue}`);
+//     return accumulator+currentValue;
+// } ,0)
+
+
+// const myTotal = myNum.reduce ( (acc, currVal) => acc+currVal, 0 );
+
+// console.log(myTotal)
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    }
+]
+
+// const myCost = shoppingCart.reduce( (acc, item) => acc + item.price, 0 );
+// console.log(myCost);
